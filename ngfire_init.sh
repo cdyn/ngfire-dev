649c89a4004dddc22dd274041329634e5fc6e3ff
Jean Runnells <jrunnells@customdyn.com>
2023-10-30 08:47:35 -0400

    .sh control flow

--- ngfire_init.sh
+++ ngfire_init.sh
@@ -1,18 +1,29 @@
 # Install required packages
 apt-get update && 
-apt-get install -y git gh nodejs npm &&
+apt-get install -y git gh nodejs npm tree &&
 npm install -g firebase-tools && 
 npm install -g @angular/cli &&
 
 # Log in to Github and prompt the user for the repository to clone
 gh auth login &&
 gh auth setup-git &&
-echo "Enter a GitHub repository URL to clone and existing repo: " && 
+firebase login &&
+echo "Enter a GitHub repository URL to clone an existing repo: " && 
 read repo_url && 
 if git clone $repo_url . ; then
-    
-else 
-    git repo create &&
+    # prompt for Angular application root
+    echo "Enter the relative path to the Angular application root: " && 
+    tree -d
+    read app_path && 
+    cd $app_path &&
+    npm install &&
+else
+    # prompt for a new repo name
+    echo "Enter a name for the new repository: " &&
+    read repo_name &&
+    mkdir $repo_name &&
+    cd $repo_name &&
+    ng new
 fi
 # log into initialize the Firebase and the project
 firebase login &&
@@ -20,6 +31,7 @@ firebase init &&
 
 # prompt for Angular application root
 echo "Enter the relative path to the Angular application root: " && 
+tree -d
 read app_path && 
 cd $app_path &&
 npm install angularfire
