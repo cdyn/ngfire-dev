3259a972481a82feb4ddd8bc7a924cd41637acfd
Jean Runnells <jrunnells@customdyn.com>
2023-10-31 02:14:08 -0400

    .sh

--- ngfire_init.sh
+++ ngfire_init.sh
@@ -8,12 +8,13 @@ npm install -g @angular/cli &&
 gh auth login &&
 gh auth setup-git &&
 firebase login &&
-echo "Enter a GitHub repository URL to clone an existing repo (leave blank to create ): " && 
+echo "Enter a GitHub repository URL to clone an existing repo (leave blank to create new): " && 
 read repo_url && 
 
 # Clone the repository or prompt for a new repo name
 if git clone $repo_url ; then
     # prompt for Angular application root
+    firebase init &&
     echo "Enter the relative path to the Angular application root: " && 
     tree -d
     read app_path && 
@@ -25,7 +26,18 @@ else
     read repo_name &&
     mkdir $repo_name &&
     cd $repo_name &&
+    echo "Would you like to create the reository in an organization? (enter the org ID if so)" &&
+    gh org list &&
+    read org_id &&
+    if [$org_id]; then
+        gh repo create $orgid/$repo_name --clone
+    else
+        gh repo create $repo_name --clone
+    fi
+    gh repo create $orgid/--clone 
+    firebase init &&
     ng new
+    npm install @angluar/fire &&
 fi
 # log into initialize the Firebase and the project
 firebase login &&
