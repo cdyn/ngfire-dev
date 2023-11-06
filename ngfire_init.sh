6fe51e9763a6d881562527cd53bf61264c5373e0
Jean Runnells <jrunnells@customdyn.com>
2023-11-05 21:36:47 -0500

    ngfire_init.sh

--- ngfire_init.sh
+++ ngfire_init.sh
@@ -14,21 +14,21 @@ echo "Enter a GitHub repository URL to clone an existing repo (leave blank to cr
 read repo_url && 
 
 # Clone the repository or prompt for a new repo name
-if [$repo_url] && git clone $repo_url && ; then
+if [$repo_url] && git clone $repo_url ; then
     cd "$(basename "$repo_url" .git)" &&
     # prompt for Angular application root
     firebase init &&
-    echo "Enter the relative path to the Angular application root: "  
+    echo "Enter the relative path to the Angular application root (typically /public/{app_name}): "  
     tree -d
     read app_path && 
     cd $app_path &&
     npm install &&
 else if [! $repo_url] ; then
     # prompt for a new repo name
-    echo "Enter a name for the new repository: " &&
+    echo "Enter a name for the new repository: " 
     read repo_name &&
     echo "Would you like to create the reository in an organization? (enter the org ID, leave blank otherwise):" 
-    gh org list &&
+    gh org list 
     read org_id &&
     if [$org_id]; then
         gh repo create $orgid/$repo_name --clone --private &&
@@ -37,13 +37,16 @@ else if [! $repo_url] ; then
     fi
     cd $repo_name &&
     firebase init &&
-    echo "Enter name the Angular app: " &&
+    echo "Enter name the Angular app: " 
     read ng_id &&
     cd "/public" &&
-    ng new $ng_id &&
+    ng new $ng_id --skip-git &&
+    cd $ng_id &&
     npm install @angluar/fire &&
 else
     # bad url
     echo "Invalid url. Exiting." 
     exit 1
 fi
+cd ../..
+exit 0
